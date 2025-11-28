d7c78e0ffc2bcf6df92f165bbd3a04f976d2448b
ALLE KEERTHI HARSHA <130027038+KEERTHI130705@users.noreply.github.com>
2025-11-28 20:05:32 +0530

    Update event deadlines for early bird and regular

--- frontend/src/config/eventConfig.js
+++ frontend/src/config/eventConfig.js
@@ -1,2 +1,2 @@
-export const EARLY_BIRD_END_DATE = new Date("2025-11-30T23:59:59"); //early bird deadline
-export const REGULAR_END_DATE = new Date("2025-12-30T23:59:59"); //regular deadline
+export const EARLY_BIRD_END_DATE = new Date("2025-12-15T23:59:59"); //early bird deadline
+export const REGULAR_END_DATE = new Date("2026-1-1T23:59:59"); //regular deadline
